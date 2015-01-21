8d2d0b0644ee2fce8be281948892b4aa7c4f6ede
Kevin.Li <li_zhaoyu@msn.com>
2015-01-21 15:25:49 +0800

    fix

---
+++ style_config.py
@@ -0,0 +1,46 @@
+ # -*- coding: utf-8 -*-
+__author__ = 'zhaoyuli'
+html_header = """
+<html>
+<head>
+    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<style>
+body { background:#fff; -webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+
+/*禁止iphone字体放大 */
+html { -webkit-text-size-adjust: none; }
+
+
+
+body{ width:100%; min-width:320px; font-size:1em; font-family:"Hiragino Sans GB W3";}
+.container{ width:100%; height:auto;}
+.parking-title{ font-size:1em; text-align:center; height:60px; line-height:60px;}
+.parking-table{ height:auto; text-align:center; margin:0 10px 20px; -webkit-border-radius:5px;-moz-border-radius:5px;-o-border-radius:5px;-ms-border-radius:5px;}
+.parking-table table{ width:100%; height:auto; border:1px solid #e0e0e0; -webkit-border-radius:5px;-moz-border-radius:5px;-o-border-radius:5px;-ms-border-radius:5px; margin-bottom:10px;}
+.parking-table thead{ width:100%; height:40px; line-height:40px; color:#fff; font-size:1em; background:#19add9;}
+.parking-table thead tr td{ border-top:3px solid #0c7cb1;}
+.parking-table thead tr td:first-child{ -webkit-border-radius:5px 0 0 0;-moz-border-radius:5px 0 0 0;-o-border-radius:5px 0 0 0;-ms-border-radius:5px 0 0 0;}
+.parking-table thead tr td:last-child{ -webkit-border-radius:0 5px 0 0;-moz-border-radius:0 5px 0 0;-o-border-radius:0 5px 0 0;-ms-border-radius:0 5px 0 0;}
+.parking-table tbody{ width:100%; height:auto;}
+.parking-table tbody td{ font-size:1em; color:#19add9; line-height:40px; padding:10px; border:1px solid #e0e0e0;}
+.parking-table tbody tr:last-child td:first-child{ -webkit-border-radius:0 0 0 5px;-moz-border-radius:0 0 0 5px;-o-border-radius:0 0 0 5px;-ms-border-radius:0 0 0 5px;}
+.parking-table tbody tr:last-child td:last-child{ -webkit-border-radius:0 0 5px 0;-moz-border-radius:0 0 5px 0;-o-border-radius:0 0 5px 0;-ms-border-radius:0 0 5px 0;}
+.parking-table tbody td .red{ color:#d73d32;}
+
+.btn-fresh{ width:120px; height:35px; background:#19add9; font-size:1em; color:#fff; border:1px solid #2580b7; border-radius:5px; box-shadow:inset 0 0 5px #1bafff;}
+.btn-fresh img{ margin-left:2px;margin-bottom:2px; vertical-align:middle; border:none;}
+
+.footer{ width:100%; height:auto; text-align:center; padding-bottom:10px;}
+.footer p{ line-height:30px; font-size:1em; color:#9a9a9a;}
+a{ text-decoration:underline;}
+</style>
+</head>
+<body>
+"""
+html_ender = """
+</body>
+</html>
+"""
