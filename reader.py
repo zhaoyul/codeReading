44719ba290d6a39e6cfc9686134da9cfc3a1c7d8
Kevin.Li <li_zhaoyu@msn.com>
2015-01-21 15:37:32 +0800

    changes

--- reader.py
+++ reader.py
@@ -1,10 +1,8 @@
  # -*- coding: utf-8 -*-
 __author__ = 'zhaoyuli'
-import Image
-from StringIO import StringIO
 import requests
-from bs4 import BeautifulSoup
 import web
+import json
 
 """
 测试方式：http://127.0.0.1:8080/wzAPI?stateid=B&plateNo=7f128&license=0477
@@ -35,48 +33,11 @@ class Hello:
     def GET(self, name):
         result_list = []
 
-        def get_weizhang_info(state_id, plate_no, license_no):
-            s = requests.Session()
-            r = s.get("http://218.58.65.23/select/WZ.asp")
-            yzr=s.get('http://218.58.65.23/select/checkcode.asp')
-            im = Image.open(StringIO(yzr.content))
-            # debug only
-            #im.show()
-
-            image_list = cut_pictures(im)
-            code_text = read_pics(image_list)
-
-            payload = {'stateid':state_id,'hphm':plate_no, 'hpzl':'02', 'jzh':license_no, 'yam':code_text, 'image.x':'-583', 'image.y':'-374'}
-            r = s.post("http://218.58.65.23/select/WZ.asp",data=payload)
-            r.encoding='gb2312'
-            #print r.text
-            parsed_html = BeautifulSoup(r.text)
-            html = parsed_html.prettify().encode('utf-8')
-            if r'请输入正确的验证码' in html:
-                return get_weizhang_info(state_id, plate_no, license_no)
-            # Find the specific table, rather than the whole one.
-            data_table = parsed_html.find_all("table",  {"width":"100%", "align":"center", "border":"0", "cellspacing":"0", "cellpadding":0  })
-
-            if len(data_table) < 2:
-                return None
-            rows = data_table[2].findAll('tr')
-            for tr in rows:
-                cols = tr.findAll('td', {"class": "css"})
-                result_list.append([c.text for c in cols])
-
-
-            weizhang_tables = parsed_html.find_all("table",  {"width":5, "align":"center", "border":"0", "cellspacing":"0", "cellpadding":0  })
-            for weizhang_table in weizhang_tables:
-                for weizhang_row in weizhang_table.find_all('tr'):
-                    cols = weizhang_row.findAll('td')
-                    if len(cols) == 7:
-                        result_list.append([c.text for c in cols])
-
-
-            return 'successful'
-
-
         web.header('Content-Type', 'text/html; charset=utf-8')
+
+
+
+
         ret_str=''
         user_data = web.input()
         # print user_data
@@ -92,7 +53,22 @@ class Hello:
             web.setcookie('license_id', license_id, 3600000)
         except:
             ret_str = r"""输入格式错误，请重按照说明新输入"""
-        ret_str = str(get_weizhang_info(state_id, plate_id, license_id))
+        # ret_str = str(get_weizhang_info(state_id, plate_id, license_id))
+        url = "http://sixinone.qdznjt.com/sixinone/services/queryObjectOut"
+        headers = {'content-type': "application/json",
+                   "xtlb": "aiqingdao",
+                   "jkid": "04C01",
+                   "authCode": "53ea94bd0c68477685be10287f2c3867"}
+        payload = {
+            "hpzl": "02",
+            "hphm": "鲁B789CA",
+            "clsbdh": "6170",
+            "sjhm": "18888888888",
+            "startNum": 1,
+            "endNum": 5
+        }
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
+
         # ret_str = html_header + ret_str + html_ender
         if ret_str == 'None':
             ret_str = r'输入格式错误，请重新按照说明输入'
@@ -101,81 +77,8 @@ class Hello:
         return render.result(result_list)
 
 
-picDict = {'0': Image.open('./codepic/0.png'),
-           '1': Image.open('./codepic/1.png'),
-           '2': Image.open('./codepic/2.png'),
-           '3': Image.open('./codepic/3.png'),
-           '4': Image.open('./codepic/4.png'),
-           '5': Image.open('./codepic/5.png'),
-           '6': Image.open('./codepic/6.png'),
-           '7': Image.open('./codepic/7.png'),
-           '8': Image.open('./codepic/8.png'),
-           '9': Image.open('./codepic/9.png'),
-           'A': Image.open('./codepic/a.png'),
-           'B': Image.open('./codepic/b.png'),
-           'C': Image.open('./codepic/c.png'),
-           'D': Image.open('./codepic/d.png'),
-           'E': Image.open('./codepic/e.png'),
-           'F': Image.open('./codepic/f.png'),
-           'G': Image.open('./codepic/g.png'),
-           'H': Image.open('./codepic/h.png'),
-           'I': Image.open('./codepic/i.png'),
-           'J': Image.open('./codepic/j.png'),
-           'K': Image.open('./codepic/k.png'),
-           'L': Image.open('./codepic/l.png'),
-           'M': Image.open('./codepic/m.png'),
-           'N': Image.open('./codepic/n.png'),
-           'O': Image.open('./codepic/o.png'),
-           'P': Image.open('./codepic/p.png'),
-           'Q': Image.open('./codepic/q.png'),
-           'R': Image.open('./codepic/r.png'),
-           'S': Image.open('./codepic/s.png'),
-           'T': Image.open('./codepic/t.png'),
-           'U': Image.open('./codepic/u.png'),
-           'V': Image.open('./codepic/v.png'),
-           'W': Image.open('./codepic/w.png'),
-           'X': Image.open('./codepic/x.png'),
-           'Y': Image.open('./codepic/y.png'),
-           'Z': Image.open('./codepic/z.png')
-}
-
-def read_pics(image_list):
-    """
-    read characters from the 4 input images
-    """
-
-    code = ''
-    for image in image_list:
-        the_key = ''
-        current_max = 0
-        for key, picName in picDict.iteritems():
-            #print key
-            temp = my_image_similarity(image, picName)
-            if temp > current_max:
-                current_max = temp
-                the_key = key
-        code = code + the_key
-    print r'验证码:' + code
-    return code
 
 
-def my_image_similarity(image, character_image):
-    list1 = list(image.getdata())
-    list2 = list(character_image.getdata())
-    counter = 0
-    for i in range(len(list1)):
-        if list1[i] == list2[i]:
-            counter += 1
-    return counter
-
-def cut_pictures(img):
-    gray_img = img.convert('L').point(lambda i: i > 128 and 255)
-    frame1 = gray_img.crop(((0, 0, 10, 10)))
-    frame2 = gray_img.crop(((10, 0, 20, 10)))
-    frame3 = gray_img.crop(((20, 0, 30, 10)))
-    frame4 = gray_img.crop(((30, 0, 40, 10)))
-    image_list = [frame1, frame2, frame3, frame4]
-    return image_list
 
 
 if __name__ == '__main__':
